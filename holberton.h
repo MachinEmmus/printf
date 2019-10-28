910097c440fba0cdc8f29fc5fdb252fc0bc5e2cb
MachinEmmus <emonsalvep38@gmail.com>
2019-10-27 20:38:51 -0500

    Holberton.h add new features

--- holberton.h
+++ holberton.h
@@ -1,9 +1,30 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/*Libraries allowed*/
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
+/*Macros*/
+#define True (1 == 1)
+#define False !True
+
+/**
+ * struct kings - Structure to printf
+ * @modifier: To know as print in printf
+ * @f: Share functions
+ *
+ * Descriptions: Structure to define what is the print preformated
+ */
+
+typedef struct kings
+{
+	char modifier;
+	void (*f)(va_list);
+} kind;
+
+/*Prototypes*/
 int _printf(const char *format, ...);
 
 #endif/*HOLBERTON_H*/
