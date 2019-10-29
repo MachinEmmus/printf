5cc0c03b98395c904d3db10486bf4c155649d51d
dagute <1271@holbertonschool.com>
2019-10-29 20:52:41 +0000

    changes

--- _printf.c
+++ _printf.c
@@ -6,39 +6,39 @@
  */
 int _printf(const char *format, ...)
 {
-const char percent = '%';
-va_list Ourlist;
-unsigned int i = 0, Count = 0, k = 0;
-	kind Flags[] = {
+va_list ourlist;
+unsigned int i = 0, count = 0, k = 0, vol = 0;
+	kind flags[] = {
 			{'c', only_char    },
 			{'s', print_string },
 			{'d', c_decimal    }
 	};
-	va_start(Ourlist, format);
+	va_start(ourlist, format);
 	while (format && format[i])
 	{
-		if (format[i] == percent && format[i + 1] == percent)
-		{
-			_putchar(percent);
-			Count += 1, i += 2;
-		}
-		else if (format[i] == percent && format[i + 1] != percent)
+		vol = 0;
+		if (format[i] == '%' && format[i + 1] != '%')
 		{
 			while (k < 3)
 			{
 				while (format[i + 1] == ' ')
-					i++;
-				if (format[i + 1] == Flags[k].modifier)
-					Count += Flags[k].f(Ourlist), i += 2;
-				if (format[i + 1] == '\0')
-					return (-1);
-				k++;
+					i++, vol = 1;
+			if (format[i + 1] == flags[k].modifier)
+			{
+				count += flags[k].f(ourlist), i += 2;
+			}
+			else if (format[i + 1] == '\0')
+				return (-1);
+			k++;
 			}
 		}
-		if (format[i])
-			_putchar(format[i]), Count++, i++;
-		
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar('%'), count++, i += 2, vol = 1;
+		}
+		if (format[i] && vol == 0)
+			_putchar(format[i]), count++, i++;
 	}
-	va_end(Ourlist);
-	return (Count);
+	va_end(ourlist);
+	return (count);
 }
