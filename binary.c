aac8c368941c93906dc0f8163e0de1359c67b33c
dagute <1271@holbertonschool.com>
2019-10-31 05:32:59 +0000

    converts to binary

---
+++ binary.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_bin - turn to binary
+ * @list: argument list
+ * Return: nothing
+ */
+int print_bin(va_list list)
+{
+	int n = (va_arg(list, int));
+	int b = 0;
+	int mod;
+
+	x = 1;
+
+	while (n != 0)
+	{
+		mod = n % 2;
+		n = n / 2;
+		b = b + (mod * x);
+		x = x * 10;
+	}
+	return (print_unint(b));
+}
