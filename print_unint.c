854574b3fb95ab3ab995def31f68fe972f61a047
dagute <1271@holbertonschool.com>
2019-10-31 05:36:46 +0000

    print unsigned int numbers

---
+++ print_unint.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * print_unint - print unsigned integer
+ * @n: number
+ * Return: nothing
+ */
+int print_unint(unsigned int  n)
+{
+	unsigned int add, d, tmp, td, div;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else
+	{
+		d = 0;
+		tmp = n;
+		while (tmp != 0)
+		{
+			tmp = tmp / 10;
+			tmp++;
+		}
+		add = d;
+		td = n;
+		while (add > 0)
+		{
+			div = add - 1;
+			while (div > 0)
+			{
+			td = td / 10;
+			}
+			td = td % 10;
+			_putchar(td + '0');
+			td = n;
+			add--;
+		}
+	}
+	return (d);
+}
